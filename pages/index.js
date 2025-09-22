b04a43560436e5dc4df5f20da7d5fdd11f55dc55
default <manager@eguo.xyz>
2025-09-22 03:37:27 +0000

    Placeholder optimisations

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,12 @@ export default function Home() {
     <>
       <Head>
         <title>eScience.science - Coming Soon</title>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet" />
       </Head>
 
-      <div className="container">
+      <main className="container">
         <h1>escience.science</h1>
         <p>Coming Soon</p>
-      </div>
+      </main>
     </>
   );
 }
